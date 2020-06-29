87068f4ba1c58a7249235f4cc74bf840f771b8b6
Tomona Sako <sakotomona@gmail.com>
2020-06-28 17:02:56 -0700

    Finish project

--- MiniProjectCrawl/main.swift
+++ MiniProjectCrawl/main.swift
@@ -8,30 +8,53 @@
 
 import Foundation
 
-func readAndShowFileManager(urlPath : String, numInDir: Int) {
-    //    print("url: " + urlPath);
+func readAndShowFileManager(urlPath : String, numInDir: Int, space: String, counter: inout [Int]) {
     let url = URL(fileURLWithPath: urlPath);
     let manager = FileManager.default;
+    let spacing = "| " + space;
     do {
         let list = try manager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: .includesDirectoriesPostOrder);
         for i in 0..<list.count {
             var isDir: ObjCBool = false;
             if manager.fileExists(atPath: list[i].relativePath, isDirectory: &isDir) {
                 if isDir.boolValue {
-                    print("├" + list[i].lastPathComponent)
-                    readAndShowFileManager(urlPath: list[i].relativePath, numInDir: i - 1);
+                    counter[0] = counter[0] + 1
+                    if (space.count == 0) {
+                        print( "├─" + list[i].lastPathComponent)
+                        readAndShowFileManager(urlPath: list[i].relativePath, numInDir: i - 1, space: "  " + space, counter: &counter);
+                    } else {
+                        print(spacing + "├─" + list[i].lastPathComponent)
+                        readAndShowFileManager(urlPath: list[i].relativePath, numInDir: i - 1, space: "  |" + space, counter: &counter);
+                    }
+
                 } else {
-                    print (" ├" + list[i].lastPathComponent);
+                    counter[1] = counter[1] + 1
+                    if i == (list.count - 1) {
+                        if (space.count == 0) {
+                            print ("└─" + list[i].lastPathComponent);
+                        } else {
+                            print (spacing + "└─" + list[i].lastPathComponent);
+                        }
+                    } else {
+                        if (space.count == 0) {
+                            print ("├─" + list[i].lastPathComponent);
+                        } else {
+                            print (spacing + "├─" + list[i].lastPathComponent);
+                        }
+                    }
                 }
-            }}
+            }
+        }
     } catch {
         return
     }
-    //    if !manager.fileExists(atPath: urlPath) {
 }
 
-readAndShowFileManager(urlPath: "/Users/tomona_s/Documents/05_CICCC/DataAndAlgorithm/tameshi/", numInDir: 0 );
+var counter : [Int] = [0, 0];
+print("Type directory path :")
+let path: String = readLine()!
+print(URL(fileURLWithPath: path).lastPathComponent + " Output :")
 
+readAndShowFileManager(urlPath: path, numInDir: 0, space: "", counter: &counter );
 
-// in the MiniProjectCrawl
-//[file:///Users/tomona_s/Documents/05_CICCC/DataAndAlgorithm/MiniProjectCrawl/MiniProjectCrawl.xcodeproj/, file:///Users/tomona_s/Documents/05_CICCC/DataAndAlgorithm/MiniProjectCrawl/MiniProjectCrawl/]
+print(String(format:"%d directories, %d files", counter[0], counter[1]));
